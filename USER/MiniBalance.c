5adab96bf9719e76a94dab917a69b380815247bb
Chen-yv <gry666888@gmail.com>
2024-04-04 22:24:56 +0800

    使用了ai写了关于定时器部分的代码，未验证

--- USER/MiniBalance.c
+++ USER/MiniBalance.c
@@ -7,16 +7,9 @@
 版本：5.7
 修改时间：2021-04-29
 
-
-Brand: WHEELTEC
-Website: wheeltec.net
-Taobao shop: shop114407458.taobao.com
-Aliexpress: https://minibalance.aliexpress.com/store/4455017
-Version:5.7
-Update：2021-04-29
-
 All rights reserved
 ***********************************************/
+
 #include "stm32f10x.h"
 #include "sys.h"
 u8 Flag_Stop = 1;				   // 电机停止标志位，默认关闭
@@ -24,16 +17,30 @@ int Encoder_Left, Encoder_Right;   // 左右编码器的脉冲计数
 float Angle_Balance, Gyro_Balance; // 平衡倾角 平衡陀螺仪
 int main(void)
 {
+	int OLED_show = 1; // OLED显示标志位
 	delay_init();				// 延时函数初始化
 	JTAG_Set(JTAG_SWD_DISABLE); // 关闭JTAG接口
 	JTAG_Set(SWD_ENABLE);		// 打开SWD接口 可以利用主板的SWD接口调试
 	OLED_Init();				// OLED初始化
+
+	KEY_Init(); // 按键初始化
+
 	while (1)
 	{
 		// oled_show(); // 显示屏打开
-
-		OLED_Display_On();
-		OLED_ShowString(0, 0, "hello stm32");
-		OLED_Refresh_Gram();
+		if (click())
+		{
+			OLED_show = !OLED_show; // 切换oled显示
+		}
+		if (OLED_show)
+		{
+			OLED_Display_On();
+			OLED_ShowString(0, 0, "hello cjz");
+			OLED_Refresh_Gram();
+		}
+		else{
+			OLED_Clear();
+			OLED_Refresh_Gram();
+		}
 	}
 }
